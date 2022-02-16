0224e766799bc00b17ce4c94d961a5a372dcc992
Taylor Asprilla <taylor.asprilla@gmail.com>
2022-02-16 15:24:00 -0500

    Perfil de usuario Administrador

--- src/app/pages/perfil/perfil.component.ts
+++ src/app/pages/perfil/perfil.component.ts
@@ -1,8 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Subscription } from 'rxjs';
+import { CampoModel } from 'src/app/models/campo.model';
+import { CongregacionModel } from 'src/app/models/congregacion.model';
+import { GeneroModel } from 'src/app/models/genero.model';
+import { TipoDocumentoModel } from 'src/app/models/tipo-documento.model';
 import { UsuarioModel } from 'src/app/models/usuario.model';
+import { VacunaModel } from 'src/app/models/vacuna.model';
+import { CampoService } from 'src/app/services/campo/campo.service';
+import { CongregacionService } from 'src/app/services/congregacion/congregacion.service';
 import { FileUploadService } from 'src/app/services/file-upload/file-upload.service';
+import { GeneroService } from 'src/app/services/genero/genero.service';
+import { MinisterioService } from 'src/app/services/ministerio/ministerio.service';
+import { TipoDocumentoService } from 'src/app/services/tipo-documento/tipo-documento.service';
 import { UsuarioService } from 'src/app/services/usuario/usuario.service';
+import { VacunaService } from 'src/app/services/vacuna/vacuna.service';
 import Swal from 'sweetalert2';
 
 @Component({
@@ -10,16 +22,39 @@ import Swal from 'sweetalert2';
   templateUrl: './perfil.component.html',
   styleUrls: ['./perfil.component.css'],
 })
-export class PerfilComponent implements OnInit {
+export class PerfilComponent implements OnInit, OnDestroy {
   public perfilForm: FormGroup;
   public usuario: UsuarioModel;
   public imagenSubir: File;
   public imagenTemporal: string | ArrayBuffer;
 
+  public totalUsuarios: number = 0;
+  public usuarios: UsuarioModel[] = [];
+  public congregaciones: CongregacionModel[] = [];
+  public campos: CampoModel[] = [];
+  public tipoDocumentos: TipoDocumentoModel[] = [];
+  public vacunas: VacunaModel[] = [];
+  public generos: GeneroModel[] = [];
+
+  public camposFiltrados: CampoModel[] = [];
+
+  public congregacionSubscription: Subscription;
+  public campoSubscription: Subscription;
+  public tipoDocumentoSubscription: Subscription;
+  public usuarioSubscription: Subscription;
+  public generoSubscription: Subscription;
+  public vacunaSubscription: Subscription;
+
   constructor(
     private formBuilder: FormBuilder,
     private usuarioService: UsuarioService,
-    private fileUploadService: FileUploadService
+    private fileUploadService: FileUploadService,
+    public congregacionService: CongregacionService,
+    public campoService: CampoService,
+    public ministerioService: MinisterioService,
+    public tipoDocumentoService: TipoDocumentoService,
+    private generoService: GeneroService,
+    private vacunaService: VacunaService
   ) {
     this.usuario = usuarioService.usuario;
   }
@@ -40,6 +75,37 @@ export class PerfilComponent implements OnInit {
       imagen: [this.usuario.imagenUrl, []],
       id_congregacion: [this.usuario.id_congregacion, [Validators.required]],
     });
+
+    this.congregacionSubscription = this.congregacionService
+      .listarCongregaciones()
+      .subscribe((congregaciones: CongregacionModel[]) => {
+        this.congregaciones = congregaciones;
+      });
+
+    this.campoSubscription = this.campoService.listarCampos().subscribe((campos: CampoModel[]) => {
+      this.campos = campos;
+    });
+
+    this.tipoDocumentoSubscription = this.tipoDocumentoService
+      .listarTipoDocumento()
+      .subscribe((tipoDocumento: TipoDocumentoModel[]) => {
+        this.tipoDocumentos = tipoDocumento;
+      });
+
+    this.vacunaSubscription = this.vacunaService.listarVacunas().subscribe((vacuna: VacunaModel[]) => {
+      this.vacunas = vacuna;
+    });
+
+    this.generoSubscription = this.generoService.listarGenero().subscribe((genero: GeneroModel[]) => {
+      this.generos = genero;
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.tipoDocumentoSubscription?.unsubscribe();
+    this.campoSubscription?.unsubscribe();
+    this.congregacionSubscription?.unsubscribe();
+    this.usuarioSubscription?.unsubscribe();
   }
 
   actualizarPerfil() {
@@ -61,7 +127,7 @@ export class PerfilComponent implements OnInit {
   cambiarImagen(file: File) {
     this.imagenSubir = file;
 
-    const extensionesValidas = ['png', 'jpg', 'jpeg', 'gif'];
+    const extensionesValidas = ['png', 'jpg', 'jpeg', 'pdf', 'PDF', 'PNG', 'JPEG', 'JPG'];
     if (!extensionesValidas.includes(file.name.split('.').pop())) {
       Swal.fire('Error', `El fichero no contiene una extension válida ( ${extensionesValidas} )`, 'error');
       return (this.imagenTemporal = null);
@@ -87,5 +153,9 @@ export class PerfilComponent implements OnInit {
         Swal.fire('Imagen', 'No se pudo subir la imagen', 'error');
       }
     });
+  }
+
+  listarCampos(congregacion: string) {
+    this.camposFiltrados = this.campos.filter((campoBuscar) => campoBuscar.id_congregacion === parseInt(congregacion));
   }
 }
