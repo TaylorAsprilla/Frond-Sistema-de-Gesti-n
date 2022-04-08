fb8954efe06a9d3ec11a133612d6a87d53c025cc
Taylor Asprilla <taylor.asprilla@gmail.com>
2022-04-08 15:41:48 -0500

    Omite los ceros de la izquierda

--- src/app/components/carnet-vacunacion/carnet-vacunacion.component.ts
+++ src/app/components/carnet-vacunacion/carnet-vacunacion.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
 import { UsuarioModel } from 'src/app/models/usuario.model';
 
 @Component({
@@ -6,10 +6,28 @@ import { UsuarioModel } from 'src/app/models/usuario.model';
   templateUrl: './carnet-vacunacion.component.html',
   styleUrls: ['./carnet-vacunacion.component.scss'],
 })
-export class CarnetVacunacionComponent implements OnInit {
+export class CarnetVacunacionComponent implements OnInit, OnChanges {
   @Input() busquedaUsuario: UsuarioModel[] = [];
+
+  ingreso: boolean = false;
 
   constructor() {}
 
   ngOnInit(): void {}
+
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log(changes.busquedaUsuario);
+    // console.log(changes.busquedaUsuario.currentValue[0].id_dosis >= 2);
+    if (changes.busquedaUsuario) {
+      if (
+        (changes.busquedaUsuario.currentValue[0]?.id_vacuna === 4 ||
+          changes.busquedaUsuario.currentValue[0]?.id_dosis >= 2) &&
+        !!changes.busquedaUsuario.currentValue[0]?.carnet
+      ) {
+        this.ingreso = true;
+      } else {
+        this.ingreso = false;
+      }
+    }
+  }
 }
