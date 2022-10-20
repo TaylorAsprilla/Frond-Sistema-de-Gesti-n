7748bcfebc967c1457543e1596f9a3c140e50b7d
Taylor Asprilla <taylor.asprilla@gmail.com>
2022-10-20 13:08:17 -0500

    Se eliminan las restricciones de ingresos

--- src/app/components/carnet-vacunacion/carnet-vacunacion.component.ts
+++ src/app/components/carnet-vacunacion/carnet-vacunacion.component.ts
@@ -1,9 +1,9 @@
-import { Component, EventEmitter, Input, OnChanges, Output, SimpleChanges } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { Router } from '@angular/router';
-import * as moment from 'moment';
+
 import { IngresoModel } from 'src/app/models/ingreso.model';
 import { UsuarioModel } from 'src/app/models/usuario.model';
-import { Dosis, RangosDeEdad, Vacuna } from 'src/app/models/vacuna.model';
+
 import { IngresoService } from 'src/app/services/ingreso/ingreso.service';
 import Swal from 'sweetalert2';
 
@@ -12,48 +12,17 @@ import Swal from 'sweetalert2';
   templateUrl: './carnet-vacunacion.component.html',
   styleUrls: ['./carnet-vacunacion.component.scss'],
 })
-export class CarnetVacunacionComponent implements OnChanges {
+export class CarnetVacunacionComponent {
   @Input() busquedaUsuario: UsuarioModel[] = [];
   @Input() todosLosUsuarios: UsuarioModel[] = [];
   @Input() congregacionIngreso: string = '';
   @Input() ingresos: IngresoModel[] = [];
   @Output() onIngresoUsuario = new EventEmitter<string>();
 
-  ingreso: boolean = false;
   voluntario: UsuarioModel;
   fecha = new Date().toLocaleDateString('en-CA');
 
   constructor(private ingresoServices: IngresoService, private router: Router) {}
-
-  ngOnChanges(changes: SimpleChanges): void {
-    if (changes.busquedaUsuario) {
-      let edad = this.validarFecha(changes.busquedaUsuario.currentValue[0]?.fecha_nacimiento);
-
-      if (
-        (changes.busquedaUsuario.currentValue[0]?.id_vacuna === Vacuna.JOHNSON_JOHNSON ||
-          changes.busquedaUsuario.currentValue[0]?.id_vacuna === Vacuna.SINOPHARM ||
-          changes.busquedaUsuario.currentValue[0]?.id_dosis >= Dosis.DOS_DOSIS) &&
-        !!changes.busquedaUsuario.currentValue[0]?.carnet
-      ) {
-        this.ingreso = true;
-      } else if (
-        edad > RangosDeEdad.SIN_VACUNA &&
-        edad <= RangosDeEdad.UNA_VACUNA &&
-        changes.busquedaUsuario.currentValue[0]?.carnet
-      ) {
-        this.ingreso = true;
-      } else if (edad <= RangosDeEdad.SIN_VACUNA) {
-        this.ingreso = true;
-      } else {
-        this.ingreso = false;
-      }
-    }
-  }
-
-  validarFecha(fecha: Date) {
-    const years = Math.floor(moment().diff(moment(fecha), 'years', true));
-    return years;
-  }
 
   darIngreso() {
     let idVoluntario = localStorage.getItem('idUsuario');
